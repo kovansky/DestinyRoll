d600cc497dbf6e384f4bbe48709bf7573d440755
kovansky <root@f4dev.me>
2020-04-20 21:40:41 +0200

    feat: created prepareParser method
    Registered -t (token) and -p (prefix) arguments

--- src/main/java/me/f4dev/destinyroll/DestinyRoll.java
+++ src/main/java/me/f4dev/destinyroll/DestinyRoll.java
@@ -1,7 +1,24 @@
 package me.f4dev.destinyroll;
 
+import org.apache.commons.cli.*;
+
 public class DestinyRoll {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
+    CommandLine cmd = prepareParser(args);
+    
+    
+  }
   
+  private static CommandLine prepareParser(String[] args) throws ParseException {
+    Options options = new Options();
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd;
+    
+    options.addOption("t", "token", true, "Discord bot token.");
+    options.addOption("p", "prefix", false, "Messages prefix.");
+  
+    cmd = parser.parse(options, args);
+    
+    return cmd;
   }
 }
