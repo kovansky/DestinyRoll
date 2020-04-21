a859f15a7781867dc77256af5708cc6a023404f4
Stanisław Kowański <skowanski@basecode.pro>
2020-04-21 13:03:08 +0200

    feat: connecting to Bot Instance

--- src/main/java/me/f4dev/destinyroll/DestinyRoll.java
+++ src/main/java/me/f4dev/destinyroll/DestinyRoll.java
@@ -1,14 +1,28 @@
 package me.f4dev.destinyroll;
 
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
 import org.apache.commons.cli.*;
 
+import javax.security.auth.login.LoginException;
+
 public class DestinyRoll {
-  public static void main(String[] args) throws ParseException {
+  public static void main(String[] args) throws ParseException, LoginException {
     CommandLine cmd = prepareParser(args);
+    String token = "", prefix;
+    JDABuilder builder = new JDABuilder(AccountType.BOT);
     
     if(!cmd.hasOption("t")) {
       System.err.println("You have to pass the token (-t, -token)");
+      System.exit(1);
+    } else {
+      token = cmd.getOptionValue('t');
     }
+    
+    prefix = cmd.getOptionValue('p', "!");
+    
+    builder.setToken(token);
+    builder.build();
   }
   
   private static CommandLine prepareParser(String[] args) throws ParseException {
