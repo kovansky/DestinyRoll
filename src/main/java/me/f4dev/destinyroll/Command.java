1e6e979c4d1f7cbe759e0d75d7b334bbc00baedf
kovansky <root@f4dev.me>
2020-05-08 16:47:13 +0200

    feat: type of Command changed from interface to class. Added constructors, getters, setters and helper methods for arguments list.

--- src/main/java/me/f4dev/destinyroll/Command.java
+++ src/main/java/me/f4dev/destinyroll/Command.java
@@ -2,15 +2,87 @@ package me.f4dev.destinyroll;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
-public interface Command {
-  String name = "";
-  String shortname = "";
+public class Command {
+  String name;
+  String shortname;
   Module module = null;
   
   /**
    * ArrayList of arguments (CommandArgument)
    */
-  ArrayList<CommandArgument> args = new ArrayList<>();
+  LinkedHashMap<String, CommandArgument> args = new LinkedHashMap<>();
+  
+  public Command(String name, String shortname, Module module, LinkedHashMap<String, CommandArgument> args) {
+    this.name = name;
+    this.shortname = shortname;
+    this.module = module;
+    this.args = args;
+  }
+  
+  public Command(String name, String shortname, Module module) {
+    this.name = name;
+    this.shortname = shortname;
+    this.module = module;
+  }
+  
+  public Command(String name, String shortname) {
+    this.name = name;
+    this.shortname = shortname;
+  }
+  
+  public Command(String name) {
+    this.name = name;
+    this.shortname = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getShortname() {
+    return shortname;
+  }
+  
+  public void setShortname(String shortname) {
+    this.shortname = shortname;
+  }
+  
+  public Module getModule() {
+    return module;
+  }
+  
+  public void setModule(Module module) {
+    this.module = module;
+  }
+  
+  public LinkedHashMap<String, CommandArgument> getArgs() {
+    return args;
+  }
+  
+  public void setArgs(LinkedHashMap<String, CommandArgument> args) {
+    this.args = args;
+  }
+  
+  public void addArg(String name, CommandArgument argument) {
+    args.put(name, argument);
+  }
+  
+  public void addArg(CommandArgument argument) {
+    this.addArg(argument.getName(), argument);
+  }
+  
+  public CommandArgument getArg(String name) {
+    return args.getOrDefault(name, null);
+  }
+  
+  public void removeArg(String name) {
+    args.remove(name);
+  }
 }
